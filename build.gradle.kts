8538d6488b3b489d9c84f86dab4e423baa5f7782
Jocke <joakim.lahtinen@outlook.com>
2024-01-05 13:39:21 +0100

    Update kotlin, java and gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.6.21"
+    kotlin("jvm") version "1.9.22"
     application
 }
 
@@ -21,7 +21,7 @@ tasks.test {
 }
 
 tasks.withType<KotlinCompile>() {
-    kotlinOptions.jvmTarget = "1.8"
+    kotlinOptions.jvmTarget = "21"
 }
 
 application {
